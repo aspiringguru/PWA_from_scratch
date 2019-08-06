dfbb9529fd9946ecf43a4bb9500f4c9f5a431585
aspiringguru <bmatthewtaylor@gmail.com>
2019-08-06 13:26:11 +1000

    selector added. not interactive yet

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 //nb: API_KEY is imported from config.js by the html file.
 console.log("apiKey:", apiKey)
 const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sources');
 
 window.addEventListener('load', e => {
     updateNews();
+    updateSources();
 });
 
+async function updateSources() {
+    const res = await fetch('https://newsapi.org/v1/sources')
+    const json = await res.json();
+    sourceSelector.innerHTML = json.sources
+        .map(source => `<option value="${source.id}">${source.name}</option>`)
+        .join('\n');
+}
 
 async function updateNews() {
     const res = await fetch('https://newsapi.org/v1/articles?source=techcrunch&apiKey='.concat(apiKey))
